4f75f0683b2d0bb5735f62a0fd1cfd512ec512d3
SKUM174 <sunny.kumar@albertsons.com>
2026-04-30 12:12:33 +0530

    initial docker setup

---
+++ src/main/java/com/sun/awsprac/AwspracApplication.java
@@ -0,0 +1,13 @@
+package com.sun.awsprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwspracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwspracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sun/awsprac/AwspracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.awsprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwspracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
