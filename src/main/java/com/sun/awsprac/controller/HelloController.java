c3d671f311b4ee92b03634e3f8876ba9a3fed510
SKUM174 <sunny.kumar@albertsons.com>
2026-05-01 09:57:06 +0530

    controller udpated

--- src/main/java/com/sun/awsprac/controller/HelloController.java
+++ src/main/java/com/sun/awsprac/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.sun.awsprac.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 public class HelloController {
@@ -9,5 +11,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello words updated deployment  version 3.0";
+    }
+
+    @GetMapping("/hellow")
+    public String hellow() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return "Latest fresh response at: " + now.format(formatter) + " - Version 3.0";
     }
 }
